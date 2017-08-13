378051b55d122a4d8ccddc4a00bbfefd8a83a64a
Eric <spb828@naver.com>
2017-08-14 02:25:23 +0900

    Update Model and View, Init CocoaPods

---
+++ CanYou/CanYou/ViewController/DashBoardViewController.swift
@@ -0,0 +1,52 @@
+//
+//  DashBoardViewController.swift
+//  CanYou
+//
+//  Created by Macbook Pro on 2017. 8. 13..
+//  Copyright © 2017년 Eric Park. All rights reserved.
+//
+
+import UIKit
+
+class DashBoardViewController: UIViewController {
+    let dataController = DataController.sharedDataController
+
+//    @IBOutlet var navigationItem: UINavigationItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Hide the navigation bar on the this view controller
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Show the navigation bar on other view controllers
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios
+project 'CanYou/CanYou'
+workspace './CanYou.xcworkspace'
+

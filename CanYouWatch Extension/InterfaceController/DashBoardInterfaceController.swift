f84735b2601b26857fc1cd7e962328a3b60549be
Eric <spb828@naver.com>
2017-08-20 08:37:44 +0900

    WatchOS Animation Test

--- CanYouWatch Extension/InterfaceController/DashBoardInterfaceController.swift
+++ CanYouWatch Extension/InterfaceController/DashBoardInterfaceController.swift
@@ -11,21 +11,25 @@ import Foundation
 
 
 class DashBoardInterfaceController: WKInterfaceController {
-
+    @IBOutlet var group: WKInterfaceGroup!
+    let duration = 1.2
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        // Configure interface objects here.
-    }
+        print("1")
 
+        group.setBackgroundImageNamed("Test")
+        group.startAnimatingWithImages(in: NSMakeRange(0, 35), duration: self.duration, repeatCount: 1)
+    }
+    
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
-
+    
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-
+    
 }
